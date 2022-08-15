84cb17a6174628ffa4210636ae72bae422fce040
MetalTurtle <dekolis1218@gmail.com>
2022-08-14 22:29:22 -0700

    Add page transitions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,30 @@
+<script context='module'>
+    export const load = ({ url }) => {
+        const currentRoute = url.pathname
+
+        return {
+            props: {
+                currentRoute
+            }
+        }
+    }
+</script>
+
 <script>
     import Header from '$lib/components/Header.svelte'
     import Footer from '$lib/components/Footer.svelte'
     import '$lib/styles/style.scss'
+    import { fade } from 'svelte/transition'
+
+    export let currentRoute
 </script>
 
 <Header />
 
-<main>
-    <slot />
-</main>
+{#key currentRoute}
+    <main in:fade={{duration: 150}}>
+        <slot />
+    </main>
+{/key}
 
 <Footer />
